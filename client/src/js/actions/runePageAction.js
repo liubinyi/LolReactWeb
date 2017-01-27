8fe82d8366c2e5d14d6ad216594181035862632c
liubinyi <bliu1@berkeley.edu>
2017-01-26 22:37:33 -0800

    added a action to get rune page by Id

--- client/src/js/actions/runePageAction.js
+++ client/src/js/actions/runePageAction.js
@@ -1,2 +1,19 @@
 //loop through runpage hash and fire up request to get all the rune stats
 //next step is to concat them together
+import axios from "axios";
+
+export function fetchSingleRuneStats(runeId) {
+
+  return function(dispatch) {
+
+  	const url = "https://global.api.pvp.net/api/lol/static-data/na/v1.2/rune/" + runeId +"?locale=en_US&runeData=stats&api_key=2b7a32bd-a9e9-4610-9f46-01b8a881e632";
+
+    axios.get(url)
+      .then((response) => {
+        dispatch({type: "LOAD_SINGLE_RUNE_STATS_DATA_SUCCESS", payload: response.data})
+      })
+      .catch((err) => {
+        dispatch({type: "LOAD_SINGLE_RUNE_STATS_DATA_ERROR", payload: err})
+      })
+  }
+}
