932544fc7806416e7dfe89f0d06c55df9ea8742f
liubinyi <bliu1@berkeley.edu>
2017-02-26 15:20:57 -0800

    switching the last piece to golang

--- client/src/js/actions/itemStatsAction.js
+++ client/src/js/actions/itemStatsAction.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export function fetchItemStats() {
 
   return function(dispatch) {
-    axios.get("https://global.api.pvp.net/api/lol/static-data/na/v1.2/item?locale=en_US&itemListData=depth,maps,stats,tags&api_key=2b7a32bd-a9e9-4610-9f46-01b8a881e632")
+    axios.get("https://lolyasuo.herokuapp.com/allItem")
       .then((response) => {
         dispatch({type: "LOAD_ITEM_STATS_DATA_SUCCESS", payload: response.data.data})
       })
