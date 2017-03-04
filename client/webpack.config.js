162f9b75b5149368bc0cdd15f610c5f27809d20b
liubinyi <bliu1@berkeley.edu>
2017-03-04 11:18:08 -0800

    disable warning if it’s production

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -27,6 +27,11 @@ module.exports = {
   plugins: debug ? [] : [
     new webpack.optimize.DedupePlugin(),
     new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.DefinePlugin({
+      "process.env": { 
+         NODE_ENV: JSON.stringify("production") 
+       }
+    }),
     new webpack.optimize.UglifyJsPlugin({
       minimize: true,
       compress: {
